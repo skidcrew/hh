9c157c9db107b3a02b8387bd0793b7b61ca625ba
bubatz <71530056+E8H@users.noreply.github.com>
2022-09-26 22:30:47 -0400

    Update event.js

--- event.js
+++ event.js
@@ -4,28 +4,28 @@ const body = document.getElementsByTagName('body');
 window.addEventListener("keydown", (event) => {
   if(event.ctrlKey && (event.key === "S" || event.key === "s")) {
      event.preventDefault();
-     body[0].innerHTML = "L’”"
+     body[0].innerHTML = "U FAILED THE NAZI PARTY"
   }
 
   if(event.ctrlKey && (event.key === "C")) {
      event.preventDefault();
-     body[0].innerHTML = "L’”"
+     body[0].innerHTML = "U FAILED THE NAZI PARTY"
   }
   if(event.ctrlKey && (event.key === "E" || event.key === "e")) {
      event.preventDefault();
-     body[0].innerHTML = "L’”"
+     body[0].innerHTML = "U FAILED THE NAZI PARTY"
   }
   if(event.ctrlKey && (event.key === "I" || event.key === "i" || event.key === "Ä±")) {
      event.preventDefault();
-     body[0].innerHTML = "L’”";
+     body[0].innerHTML = "U FAILED THE NAZI PARTY";
   }
   if(event.ctrlKey && (event.key === "K" || event.key === "k")) {
      event.preventDefault();
-     body[0].innerHTML = "L’”";
+     body[0].innerHTML = "U FAILED THE NAZI PARTY";
   }
   if(event.ctrlKey && (event.key === "U" || event.key === "u")) {
      event.preventDefault();
-     body[0].innerHTML = "L’”";
+     body[0].innerHTML = "U FAILED THE NAZI PARTY";
   }
 });
 // stop right click
